490d664f5f94ceb86bc4c9ebe228003c094dcc89
Ricardo Lafuente <r@manufacturaindependente.org>
2015-03-22 00:38:55 +0100

    Já dá para acrescentar novas entradas a um dataset existente; outras melhorias

--- scraper-datas_sessoes.py
+++ scraper-datas_sessoes.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import click
+import csv
 from splinter import Browser
 from bs4 import BeautifulSoup
 from collections import OrderedDict
+from zenlog import log
 
 OUTFILE = "datas_sessoes_plenarias.csv"
 
 BASE_URL = "http://debates.parlamento.pt/index.aspx?cid=r3.dar"
-MAX_LEG = 12
+LAST_LEG = 12
+LAST_SESS = 4
 WEBDRIVER = "phantomjs"
 
 # URL para uma página:
 # http://debates.parlamento.pt/page.aspx?cid=r3.dar&diary=s1l10sl2n56-0010&type=texto
 
-FIELDNAMES = ['leg', 'sess', 'number', 'session_date', 'pub_date', 'page_start', 'page_end']
+FIELDNAMES = ['leg', 'sess', 'num', 'date', 'pub_date', 'page_start', 'page_end']
 
 browser = Browser(WEBDRIVER)
 
@@ -37,33 +41,57 @@ def get_dates(leg, sess):
         entry['leg'] = leg
         entry['sess'] = sess
         try:
-            entry['number'] = int(cols[0].find("a").text)
+            entry['num'] = int(cols[0].find("a").text)
         except ValueError:
-            entry['number'] = cols[0].find("a").text
+            entry['num'] = cols[0].find("a").text
+        entry['date'] = cols[2].text
         entry['pub_date'] = cols[1].text
-        entry['session_date'] = cols[2].text
         entry['page_start'] = int(cols[3].text.split('-')[0])
         entry['page_end'] = int(cols[3].text.split('-')[1])
         entries.append(entry)
-    print "Parsed %d entries!" % len(entries)
+    log.info("Parsed %d entries!" % len(entries))
     return entries
 
 
 @click.command()
 @click.option('-a', '--scrape-all', help='Download all archives (not just most recent legislature)', is_flag=True, default=False)
-@click.option('-e', '--extend-file', help='Extend existing CSV file with new results', is_flag=True, default=False)
+@click.option('-e', '--extend-file', help='Extend existing CSV file with new results', type=click.Path(exists=True))
 def scrape(scrape_all, extend_file):
     entries = []
-    for leg in range(1, MAX_LEG + 1):
-        for sess in range(1, 5):
-            print leg, sess
-            entries.extend(get_dates(leg, sess))
-    import csv
+    if scrape_all:
+        for leg in range(1, LAST_LEG + 1):
+            for sess in range(1, 5):
+                print leg, sess
+                entries.extend(get_dates(leg, sess))
+    else:
+        entries.extend(get_dates(LAST_LEG, LAST_SESS))
+
     with open(OUTFILE, 'wb') as f:
         dw = csv.DictWriter(f, fieldnames=FIELDNAMES)
         dw.writeheader()
         for entry in entries:
             dw.writerow(entry)
+
+    if extend_file:
+        rows = open(extend_file, 'r').readlines()
+        newrows = open(OUTFILE, 'r').readlines()
+        rows_to_save = []
+        # check if each new row already exists on the file, append otherwise
+        for newrow in newrows:
+            leg, sess, num = newrow.split(',')[:3]
+            exists = False
+            for row in rows:
+                if row.startswith("%s,%s,%s" % (leg, sess, num)):
+                    exists = True
+                    break
+            if not exists:
+                log.info("New entry for %s,%s,%s, appending..." % (leg, sess, num))
+                rows_to_save.append(newrow)
+
+        if rows_to_save:
+            with open(extend_file, 'a') as f:
+                f.writelines(rows_to_save)
+        os.remove(OUTFILE)
 
     browser.quit()
 
