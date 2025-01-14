2844092588b01a8b59b905d47df3e2970acfbcde
Paulo Almeida <palmeida@lip.pt>
2025-01-14 14:47:58 +0000

    Atualizar bibliotecas e ajustar o código
    Houve uma alteração no site do Parlamento, que agora inclui a
    data de publicação dos diários em debates.parlamento.pt

--- scripts/scraper-datas-debates.py
+++ scripts/scraper-datas-debates.py
@@ -11,16 +11,16 @@ from zenlog import log
 OUTFILE = "datas-debates.csv"
 
 URL_FORMATTER = "http://debates.parlamento.pt/catalogo/r3/dar/01/%02d/%02d"
-LAST_LEG = 13
-LAST_SESS = 3
-WEBDRIVER = "phantomjs"
+LAST_LEG = 16
+LAST_SESS = 1
+WEBDRIVER = "selenium"
 
 # URL para uma página:
 # http://debates.parlamento.pt/page.aspx?cid=r3.dar&diary=s1l10sl2n56-0010&type=texto
 
-FIELDNAMES = ['leg', 'sess', 'num', 'date', 'pages', 'democratica_url', 'debates_url']
+FIELDNAMES = ['leg', 'sess', 'num', 'date', 'published_date', 'pages', 'democratica_url', 'debates_url']
 
-browser = splinter.Browser(WEBDRIVER, user_agent="Mozilla/5.0 ;Windows NT 6.1; WOW64; Trident/7.0; rv:11.0; like Gecko")
+browser = splinter.Browser(user_agent="Mozilla/5.0 ;Windows NT 6.1; WOW64; Trident/7.0; rv:11.0; like Gecko")
 
 
 def get_dates(leg, sess):
@@ -31,7 +31,10 @@ def get_dates(leg, sess):
         return entries
 
     soup = BeautifulSoup(browser.html, 'html.parser')
-    rows = soup.find('div', id="painelNumeros").find_all('tr')
+    try:
+        rows = soup.find('div', id="painelNumeros").find_all('tr')
+    except AttributeError:
+        return entries
     for row in rows:
         cols = row.find_all('td')
         if not cols:
@@ -44,7 +47,8 @@ def get_dates(leg, sess):
         except ValueError:
             num = entry['num'] = cols[0].text.strip().split(" ")[-1]
         entry['date'] = cols[1].text.strip()
-        entry['pages'] = int(cols[2].text.strip())
+        entry['published_date'] = cols[2].text.strip()
+        entry['pages'] = int(cols[3].text.strip())
         entry['democratica_url'] = "http://demo.cratica.org/sessoes/%d/%d/%s/" % (leg, sess, str(num))
         entry['debates_url'] = "http://debates.parlamento.pt" + cols[0].find("a")['href']
         entries.append(entry)
@@ -60,12 +64,12 @@ def scrape(scrape_all, extend_file):
     if scrape_all:
         for leg in range(1, LAST_LEG + 1):
             for sess in range(1, 5):
-                print leg, sess
+                print(leg, sess)
                 entries.extend(get_dates(leg, sess))
     else:
         entries.extend(get_dates(LAST_LEG, LAST_SESS))
 
-    with open(OUTFILE, 'wb') as f:
+    with open(OUTFILE, 'w') as f:
         dw = csv.DictWriter(f, fieldnames=FIELDNAMES)
         dw.writeheader()
         for entry in entries:
@@ -93,6 +97,7 @@ def scrape(scrape_all, extend_file):
         os.remove(OUTFILE)
 
     browser.quit()
+
 
 if __name__ == "__main__":
     scrape()
